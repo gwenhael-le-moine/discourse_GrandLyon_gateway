e6bc220c7e948c988dfab0afb30542b400c38660
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2018-04-20 16:02:36 +0200

    talking to discourse, use parameters for conf instead of ENV

--- app.cr
+++ app.cr
@@ -1,46 +1,95 @@
+require "base64"
+require "openssl/hmac"
+require "uri"
+
 require "kemal"
 require "crest"
 
-WD = File.dirname( Process.executable_path.to_s )
-
 ENV["PORT"] ||= "3000"
 
-["GRANDLYON_GATEWAY_KEY", "GRANDLYON_LOGIN_URL", "GRANDLYON_USERINFO_URL"].each do |key|
-  unless ENV.keys.includes?( key )
-    puts "Usage: "
-    puts "GRANDLYON_GATEWAY_KEY=\"yourKey\" GRANDLYON_LOGIN_URL=\"url\" GRANDLYON_USERINFO_URL=\"url\" ./app"
+unless ARGV.size == 4
+  puts "Usage: "
+  puts "./app \"sso key\" \"api key\" \"login_url\" \"userinfo_url\""
 
-    exit( 1 )
-  end
+  exit( 1 )
+end
+
+DISCOURSE_SSO_KEY = ARGV[0]
+GATEWAY_KEY = ARGV[1]
+LOGIN_URL = ARGV[2]
+USERINFO_URL = ARGV[3]
+
+def hash_to_payload( hash : Hash ) : String
+  hash.map { |key, value| "#{key}=#{value}" }.join( "&" )
+end
+
+def payload_to_hash( payload : String) : Hash
+  payload.split("&").map { |item| item.split("=") }.to_h
+end
+
+def sign_discourse_payload( payload : String, key : String ) : String
+  OpenSSL::HMAC.hexdigest(:sha256, key, payload)
+end
+
+def check_discourse_payload_signature( payload : String, signature : String, key : String ) : Bool
+  sign_discourse_payload( payload, key ) == signature
 end
 
 # kemal routes below
 get "/" do |env|
-  env.response.content_type = "text/html"
+  sig = env.params.query["sig"]
+  payload = env.params.query["sso"]
 
-  error_message = ""
+  if check_discourse_payload_signature( payload, sig, DISCOURSE_SSO_KEY )
+    payload = payload_to_hash( Base64.decode_string( payload ) )
+
+    error_message = ""
+    nonce = payload["nonce"]
+    return_sso_url = payload["return_sso_url"]
+  else
+    error_message = "Appel invalide de la part de Discourse."
+    nonce = "error"
+    return_sso_url = "error"
+  end
+
+  env.response.content_type = "text/html"
   render( "views/index.ecr" )
 end
 
 post "/login" do |env|
-  login = env.params.body["login"]
-  password = env.params.body["password"]
-
   begin
-    response_auth = Crest.post( ENV["GRANDLYON_LOGIN_URL"],
+    response_auth = Crest.post( LOGIN_URL,
                                 headers: { "Content-Type" => "application/x-www-form-urlencoded",
-                                           "Authorization" => "Basic #{ENV["GRANDLYON_GATEWAY_KEY"]}" },
-                                payload: "scope=openid&grant_type=password&username=#{login}&password=#{password}",
+                                           "Authorization" => "Basic #{GATEWAY_KEY}" },
+                                payload: hash_to_payload( { "scope" => "openid",
+                                                            "grant_type" => "password",
+                                                            "username" => env.params.body["login"],
+                                                            "password" => env.params.body["password"] } ),
                                 logging: true )
 
     auth = JSON.parse( response_auth.body )
-    response_info = Crest.get( ENV["GRANDLYON_USERINFO_URL"],
+    response_info = Crest.get( USERINFO_URL,
                                headers: { "Authorization" => "Bearer #{auth["access_token"]}" },
                                logging: true )
 
     user_info = JSON.parse( response_info.body )
+
+    discourse_payload = Base64.strict_encode( hash_to_payload( { "nonce" => env.params.body["nonce"],
+                                                                 "extermal_id" => user_info["email"],
+                                                                 "username" => user_info["email"],
+                                                                 "email" => user_info["email"],
+                                                                 "name" => "#{user_info["given_name"]}#{user_info["family_name"]}" } ) )
+    discourse_payload_signature = sign_discourse_payload( discourse_payload, DISCOURSE_SSO_KEY )
+
+    puts " # redirect"
+    puts "#{ URI.unescape( env.params.body["return_sso_url"] ) }?sso=#{discourse_payload}&sig=#{discourse_payload_signature}"
+    env.redirect "#{ URI.unescape( env.params.body["return_sso_url"] ) }?sso=#{discourse_payload}&sig=#{discourse_payload_signature}"
   rescue Crest::BadRequest
     error_message = "Erreur lors de l'authentification."
+    nonce = "error"
+    return_sso_url = "error"
+
+    env.response.content_type = "text/html"
     render( "views/index.ecr" )
   end
 end
